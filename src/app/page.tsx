7d992e54a5c64e523604faf1902b945bcdabde0e
fatbugsbunny <verses.v20@gmail.com>
2025-07-15 14:20:22 +0200

    Added facility section skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,14 @@ export default function Home() {
                 <Image className={``} src={`/interior2.jpg`} width={`340`} height={`340`} alt={`interior2`}></Image>
             </div>
 
-
+            <div className={`grid grid-cols-3 place-items-center`}>
+                <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
+                <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
+                <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
+                <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
+                <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
+                <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
+            </div>
 
 
         </>
