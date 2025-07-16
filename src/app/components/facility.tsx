624cdf3c729bfa1c3a715e48ead02acf0b72e5a2
fatbugsbunny <verses.v20@gmail.com>
2025-07-16 09:52:04 +0200

    Added className attribute for styling

--- src/app/components/facility.tsx
+++ src/app/components/facility.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-export default function Facility({facility, icon}: Readonly<{ facility: string, icon:string }>){
+export default function Facility({className, facility, icon}: Readonly<{ className?:string, facility: string, icon:string }>){
     return(
-        <div>
+        <div className={className}>
             <Image src={icon} width={`70`} height={`70`} alt={`facility icon`}></Image>
             <p className={`uppercase`}>{facility}</p>
         </div>
