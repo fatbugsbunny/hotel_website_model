7926e5f2f320e2c9b3d8947cdfb2ddd46fbf21a9
fatbugsbunny <verses.v20@gmail.com>
2025-07-23 15:16:53 +0200

    refactor: extract RoomCarousel from RoomCard

---
+++ src/app/rooms/components/roomCarousel.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation} from "swiper/modules";
+import Image from 'next/image'
+
+export default function RoomCarousel({photos} : Readonly<{ photos: string[] }>){
+    return (
+        <Swiper className={``} modules={[Navigation]} slidesPerView={1} navigation loop={true}>
+            {photos.map((photo, i) => (
+                <SwiperSlide key={i}>
+                    <div className={`relative h-94 w-full`}><Image className={`object-cover`} src={photo} fill
+                                                                   alt={`room image`}/></div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    )
+}
--- src/components/roomCard.tsx
+++ src/components/roomCard.tsx
@@ -1,11 +1,10 @@
-'use client'
-
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {Navigation} from "swiper/modules";
 import Image from 'next/image'
-import Link from "next/link";
 import {Jost} from "next/font/google";
 import {Room} from '@/types'
+import Link from 'next/link';
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation} from "swiper/modules";
+import RoomCarousel from "@/app/rooms/components/roomCarousel";
 
 const jost = Jost({
     subsets: ["latin"]
@@ -17,35 +16,28 @@ export default function RoomCard({className, photos, name, amenities, isInHomePa
             {isInHomePage ?
                 <div className={`relative w-20 h-20`}><Image src={photos[0]} fill alt={`room image`}/></div>
                 :
-                <Swiper className={``} modules={[Navigation]} slidesPerView={1} navigation loop={true}>
-                    {photos.map((photo, i) => (
-                        <SwiperSlide key={i}>
-                            <div className={`relative h-94 w-full`}><Image className={`object-cover`} src={photo} fill
-                                                                           alt={`room image`}/></div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+                <RoomCarousel photos={photos}/>
             }
+
             <div className={`${jost.className} grid grid-cols-1 justify-items-start p-5 font-[400] leading-[1.5]`}>
                 <p className={`text-[2.2rem] text-quinary ${jost.className} font-light`}>{name}</p>
-
-
-                <p className={`mb-4 text-[1.14rem]`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+                <p className={`mb-4 text-[1.14rem]`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Architecto
                     atque eum mollitia neque
                     temporibus, voluptas? Amet blanditiis laboriosam laborum odio quam quis tempore. Aliquam dolorem
                     facilis maxime quibusdam totam. Odio?</p>
 
                 <div className={`flex w-full justify-between text-[1.14rem]`}>
-                    <div className={`flex space-x-3 text-quinary font-medium`}>
-                        {amenities.map((amenity) => (
-                            <span className={`flex justify-center items-center space-x-2`}
-                                  key={amenity.id}> <Image src={amenity.img} width={18} height={18}
-                                                           alt={amenity.name}/> <p>{`${amenity.name}`}</p></span>))}
-                    </div>
-                    {isInHomePage ? <Link className={``} href='/rooms'>See more</Link> : <Link
-                        className={`transition py-4 px-7 rounded bg-primary text-tertiary font-medium  hover:bg-tertiary hover:text-primary `}
-                        href='/contact'>Book now</Link>}
+                <div className={`flex space-x-3 text-quinary font-medium`}>
+                    {amenities.map((amenity) => (
+                        <span className={`flex justify-center items-center space-x-2`}
+                              key={amenity.id}> <Image src={amenity.img} width={18} height={18}
+                                                       alt={amenity.name}/> <p>{`${amenity.name}`}</p></span>))}
                 </div>
+                {isInHomePage ? <Link className={``} href='/rooms'>See more</Link> : <Link
+                    className={`transition py-4 px-7 rounded bg-primary text-tertiary font-medium  hover:bg-tertiary hover:text-primary `}
+                    href='/contact'>Book now</Link>}
+            </div>
             </div>
         </div>
     )
