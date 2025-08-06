c3d94925f945cd708f90090b3263849b3e37ad40
fatbugsbunny <verses.v20@gmail.com>
2025-08-06 23:11:26 +0200

    feat: add new facilities

--- src/app/components/Facilities.tsx
+++ src/app/components/Facilities.tsx
@@ -1,14 +1,29 @@
 import Facility from "@/app/components/facility";
+import {Quicksand} from "next/font/google";
 
-export default function Facilities(){
+const quicksand = Quicksand({
+    subsets: ['latin']
+})
+
+const facilities:{name:string, icon:string}[] = [
+    {name:'private beach',icon:'/sunbed.png'},
+    {name:'lounge bar', icon:'/cheers.png'},
+    {name:'swimming pool', icon:'/swimming-pool.png'},
+    {name:'room service', icon:'/room-service.png'},
+    {name:'restaurant', icon:'/restaurant.png'},
+    {name:'free parking', icon:'/parking.png'},
+]
+
+export default function Facilities() {
     return (
-        <div className={`grid grid-cols-3 place-items-center mt-50`}>
-            <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
-            <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
-            <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
-            <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
-            <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
-            <Facility facility={`Wifi`} icon={`/wifi.png`}></Facility>
-        </div>
+        <>
+            <p className={`capitalize mt-50 text-center text-4xl text-tertiary ${quicksand.className}`}>- enjoy our services -</p>
+
+            <div className={`grid grid-cols-3 place-items-center mt-15 px-20 gap-x-4 gap-y-6 ${quicksand.className} font-[510]`}>
+                {facilities.map((v,i) => (
+                    <Facility facility={v.name} icon={v.icon} switchColor={ (i + 1) % 2 === 0} key={i}/>
+                ))}
+            </div>
+        </>
     )
 }
--- src/app/components/facility.tsx
+++ src/app/components/facility.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 
-export default function Facility({className, facility, icon}: Readonly<{ className?:string, facility: string, icon:string }>){
-    return(
-        <div className={className}>
-            <Image src={icon} width={`70`} height={`70`} alt={`facility icon`}></Image>
-            <p className={`uppercase`}>{facility}</p>
+export default function Facility({facility, icon, switchColor}: Readonly<{
+    facility: string,
+    icon: string,
+    switchColor?: boolean
+}>) {
+    return (
+        <div className={`${switchColor ? 'bg-primary text-tertiary' : 'bg-tertiary text-primary'} w-full grid grid-cols-1 transition duration-250 hover:-translate-y-1/16 hover:shadow-xl items-center p-2 mx-10`}>
+            <div className={`${switchColor ? 'border-tertiary' : 'border-primary'} border-2 p-2`}>
+                <Image src={icon} width={`64`} height={`64`} alt={`facility icon`} className={`mx-auto`}></Image>
+                <p className={`capitalize text-center text-xl`}>{facility}</p>
+            </div>
         </div>
     )
 }
