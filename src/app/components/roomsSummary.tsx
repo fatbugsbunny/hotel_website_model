719b64ac05befa76051b7f0bda404f9a9b883395
fatbugsbunny <verses.v20@gmail.com>
2025-08-15 18:37:53 +0200

    feat: replace basic images with room card component

--- src/app/components/roomsSummary.tsx
+++ src/app/components/roomsSummary.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import {Room} from "@/types";
+import {Room} from "@/components/roomCard/types";
 import {Quicksand} from "next/font/google";
-import Image from "next/image";
 import Link from "next/link";
-import {Navigation} from "swiper/modules";
+import {Autoplay, Navigation} from "swiper/modules";
 import {Swiper, SwiperSlide} from "swiper/react";
-import './mySwiper.css'
-import AmenitiesList from "@/app/rooms/components/amenitiesList";
+import './home-swiper.css'
+import RoomCard from "@/components/roomCard/roomCard";
 
 const quicksand = Quicksand({
     subsets: ['latin']
@@ -17,7 +16,7 @@ const rooms: Room[] = [
     {
         id: 1,
         name: 'Double Room',
-        photos: ['/interior1.jpg', '/interior2.jpg', '/hotel-hero-photo.jpg'],
+        photos: ['/room5.jpg', '/room2.jpg', '/room3.jpg', '/room4.jpg'],
         amenities: [{id: 0, name: 'Tv ', img: '/monitor.png'}, {
             id: 1,
             name: 'Personal wifi ',
@@ -28,7 +27,7 @@ const rooms: Room[] = [
     {
         id: 2,
         name: 'Double Room',
-        photos: ['/interior1.jpg', '/interior2.jpg', '/hotel-hero-photo.jpg'],
+        photos: ['/room5.jpg', '/room2.jpg', '/room3.jpg', '/room4.jpg'],
         amenities: [{id: 0, name: 'Tv ', img: '/monitor.png'}, {
             id: 1,
             name: 'Personal wifi ',
@@ -39,7 +38,7 @@ const rooms: Room[] = [
     {
         id: 3,
         name: 'Double Room',
-        photos: ['/interior1.jpg', '/interior2.jpg', '/hotel-hero-photo.jpg'],
+        photos: ['/room5.jpg', '/room2.jpg', '/room3.jpg', '/room4.jpg'],
         amenities: [{id: 0, name: 'Tv ', img: '/monitor.png'}, {
             id: 1,
             name: 'Personal wifi ',
@@ -50,7 +49,7 @@ const rooms: Room[] = [
     {
         id: 4,
         name: 'Double Room',
-        photos: ['/interior1.jpg', '/interior2.jpg', '/hotel-hero-photo.jpg'],
+        photos: ['/room5.jpg', '/room2.jpg', '/room3.jpg', '/room4.jpg'],
         amenities: [{id: 0, name: 'Tv ', img: '/monitor.png'}, {
             id: 1,
             name: 'Personal wifi ',
@@ -60,26 +59,26 @@ const rooms: Room[] = [
     },
 ]
 
-
 export default function RoomsSummary() {
     return (
-        <div className={`mt-30 mb-150 grid justify-items-center ${quicksand.className}`}>
+
+        <div className={`my-30 relative grid justify-items-center ${quicksand.className}`}>
             <p className={`capitalize text-center text-4xl text-tertiary ${quicksand.className}`}>- rooms & suites -</p>
-            <Swiper  className={`my-swiper w-full mt-20`} modules={[Navigation]} slidesPerView={3} navigation loop={true}>
+
+            <div className={'absolute top-1/2 -translate-y-1/2 h-60 bg-quinary w-full'}></div>
+
+            <Swiper className={`home-swiper w-full mt-10`} modules={[Navigation,Autoplay]} autoplay={{delay: 5000, disableOnInteraction:true}} slidesPerView={3} navigation pagination={{clickable: true}} loop={true}>
                 {rooms.map((room, i) => (
-                    <SwiperSlide key={i} >
-                        <div className={`grid justify-items-center`}>
-                            <div className={`relative w-[450px] h-[400px]`}>
-                                <Image src={room.photos[0]} alt={'interior'} fill className={`outline-3 p-2 mt-1 outline-tertiary relative object-cover`}/>
-                                <AmenitiesList amenities={room.amenities} className={`w-[96.5%] absolute bottom-1 left-2 bg-primary/70 text-gray-600 justify-center`}></AmenitiesList>
-                            </div>
-                            <p className={'text-2xl text-tertiary text-center p-3 capitalize  w-[450px]'}>{room.name}</p>
-                        </div>
+                    <SwiperSlide key={i}>
+                        <RoomCard className={'mx-5 mb-15 '} dimensions={'h-70 w-full'} id={room.id} photos={room.photos}
+                                  name={room.name} amenities={room.amenities} isInHomePage={true}/>
                     </SwiperSlide>
                 ))}
             </Swiper>
 
-            <Link href={`/rooms`} className={'transition py-3 px-7 rounded bg-primary text-tertiary text-[1.1rem] font-medium  hover:bg-tertiary hover:text-primary justify-self-center mt-10'}>See more</Link>
+            <Link href={`/rooms`}
+                  className={'transition py-3 px-7 rounded bg-primary text-tertiary text-[1.1rem] font-medium  hover:bg-tertiary hover:text-primary justify-self-center '}>See
+                more</Link>
         </div>
     )
 }
