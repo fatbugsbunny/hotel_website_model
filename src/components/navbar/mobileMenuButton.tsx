eeaaf45e01db5ce43ffb639d6e0ad4bfd310c13d
fatbugsbunny <verses.v20@gmail.com>
2025-08-19 17:00:50 +0200

    style: change focus outline color

--- src/components/navbar/mobileMenuButton.tsx
+++ src/components/navbar/mobileMenuButton.tsx
@@ -9,13 +9,13 @@ export default function MobileMenuButton({
 	isScrolled,
 	isOpened,
 }: Readonly<{
-	onCollapse: MouseEventHandler<HTMLAnchorElement>;
+	onCollapse: MouseEventHandler<HTMLButtonElement>;
 	isScrolled: boolean;
 	isOpened: boolean;
 }>) {
 	return (
 		<div className="lg:hidden">
-			<DisclosureButton onClick={onCollapse} className={`text-primary hover:bg-primary/10 inline-flex items-center justify-center rounded-md p-2 transition-colors hover:text-[#ffdc84] ${isScrolled ? "text-[1.2rem]" : "text-xl"}`}>
+			<DisclosureButton onClick={onCollapse} className={`text-primary focus:ring-primary focus:ring-2  focus:outline-none  hover:bg-primary/10 inline-flex items-center justify-center rounded-md p-2 transition-colors hover:text-[#ffdc84] ${isScrolled ? "text-[1.2rem]" : "text-xl"}`}>
 				<span className="sr-only">Open main menu</span>
 				{isOpened ? <X height={30} width={30} /> : <AlignJustify />}
 			</DisclosureButton>
