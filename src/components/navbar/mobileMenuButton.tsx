d2a7b2d0d8087c95d050b27703af8ee6f6bdf87d
fatbugsbunny <verses.v20@gmail.com>
2025-08-19 17:03:42 +0200

    style: remove focus outline

--- src/components/navbar/mobileMenuButton.tsx
+++ src/components/navbar/mobileMenuButton.tsx
@@ -15,7 +15,7 @@ export default function MobileMenuButton({
 }>) {
 	return (
 		<div className="lg:hidden">
-			<DisclosureButton onClick={onCollapse} className={`text-primary focus:ring-primary focus:ring-2  focus:outline-none  hover:bg-primary/10 inline-flex items-center justify-center rounded-md p-2 transition-colors hover:text-[#ffdc84] ${isScrolled ? "text-[1.2rem]" : "text-xl"}`}>
+			<DisclosureButton onClick={onCollapse} className={`text-primary focus:outline-none  hover:bg-primary/10 inline-flex items-center justify-center rounded-md p-2 transition-colors hover:text-[#ffdc84] ${isScrolled ? "text-[1.2rem]" : "text-xl"}`}>
 				<span className="sr-only">Open main menu</span>
 				{isOpened ? <X height={30} width={30} /> : <AlignJustify />}
 			</DisclosureButton>
